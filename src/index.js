6ffdb0c6a37eb6d76669fe6e0d7e7da7aaa7e8cb
rhjje <numelen@gmail.com>
2020-09-29 14:23:02 +0300

    feat: done readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
-}
+  let units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 
+  'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 
+  'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let dozens = [null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number < 20) {
+      return `${units[number]}`;
+  } else if (number > 19 && number < 100) {
+      return number % 10 === 0 ? `${dozens[Math.trunc(number / 10)]}` : `${dozens[Math.trunc(number / 10)]} ${units[number % 10]}`; 
+  } else if (number > 99 && number < 1000) {
+      if (number % 100 === 0) {
+          return `${units[number / 100]} hundred`;
+      } else if (number % 100 < 20) {
+          return `${units[Math.trunc(number / 100)]} hundred ${units[number % 100]}`;
+      } else if (number % 100 > 19 && (number % 100) % 10 === 0) {
+          return `${units[Math.trunc(number / 100)]} hundred ${dozens[(number % 100) / 10]}`;
+      } else {
+          return `${units[Math.trunc(number / 100)]} hundred ${dozens[Math.trunc((number % 100) / 10)]} ${units[(number % 100) % 10]}`;
+      }
+      
+  }
+};
+
+
+
+
+
+
+
